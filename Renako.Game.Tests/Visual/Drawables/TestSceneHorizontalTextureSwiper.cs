d366f5f551a1039b72fc55d58134949d4c740555
Phawit Pornwattanakul <me@helloyeew.dev>
2023-09-06 02:30:12 +0700

    Implement texture swiper

---
+++ Renako.Game.Tests/Visual/Drawables/TestSceneHorizontalTextureSwiper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using Renako.Game.Graphics;
+using Renako.Game.Graphics.Drawables;
+
+namespace Renako.Game.Tests.Visual.Drawables;
+
+[TestFixture]
+public partial class TestSceneHorizontalTextureSwiper : RenakoTestScene
+{
+    private List<TextureSwiperItem<int>> swiperItemList = new List<TextureSwiperItem<int>>();
+    private HorizontalTextureSwiper<int> swiper;
+    private SpriteText indexText;
+
+    [BackgroundDependencyLoader]
+    private void load(TextureStore textureStore)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            swiperItemList.Add(new TextureSwiperItem<int>()
+            {
+                Item = i,
+                Texture = textureStore.Get("Beatmaps/Album/innocence.jpg")
+            });
+        }
+
+        Add(swiper = new HorizontalTextureSwiper<int>()
+        {
+            Items = swiperItemList
+        });
+        Add(indexText = new SpriteText()
+        {
+            Anchor = Anchor.TopLeft,
+            Origin = Anchor.TopLeft,
+            Font = RenakoFont.GetFont(),
+            Text = "Index: 0"
+        });
+        swiper.BindIndexChangeAction((index) => indexText.Text = $"Index: {index.NewValue}");
+        AddAssert("Index is 0", () => swiper.CurrentIndex == 0);
+        AddStep("Swipe to next", () => swiper.Next());
+        AddAssert("Index is 1", () => swiper.CurrentIndex == 1);
+        AddStep("Swipe to previous", () => swiper.Previous());
+        AddAssert("Index is 0", () => swiper.CurrentIndex == 0);
+        AddRepeatStep("Swipe to last", () => swiper.Next(), 9);
+        AddAssert("Index is 9", () => swiper.CurrentIndex == 9);
+        AddStep("Swipe next again", () => swiper.Next());
+        AddAssert("Index is back to 0", () => swiper.CurrentIndex == 0);
+
+        // TODO: Add more tests
+    }
+}
---
+++ Renako.Game/Graphics/Drawables/HorizontalTextureSwiper.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Graphics.UserInterface;
+using osuTK;
+
+namespace Renako.Game.Graphics.Drawables;
+
+public partial class HorizontalTextureSwiper<T> : CompositeDrawable
+{
+    private Button chevronLeftContainer;
+    private Button chevronRightContainer;
+
+    private TextureSwiperContainer leftContainer2;
+    private TextureSwiperContainer leftContainer1;
+    private TextureSwiperContainer centerContainer;
+    private TextureSwiperContainer rightContainer1;
+    private TextureSwiperContainer rightContainer2;
+
+    public List<TextureSwiperItem<T>> Items { get; set; } = new List<TextureSwiperItem<T>>();
+
+    private BindableInt currentIndex = new BindableInt();
+    public readonly Bindable<T> CurrentItem = new Bindable<T>();
+
+    public HorizontalTextureSwiper()
+    {
+        Anchor = Anchor.BottomCentre;
+        Origin = Anchor.BottomCentre;
+        RelativeSizeAxes = Axes.X;
+        Size = new Vector2(1, 150);
+        Position = new Vector2(0, -115);
+    }
+
+    [BackgroundDependencyLoader]
+    private void load(TextureStore textureStore)
+    {
+        InternalChildren = new Drawable[]
+        {
+            new Box()
+            {
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                RelativeSizeAxes = Axes.Both,
+                Colour = Color4Extensions.FromHex("82767E")
+            },
+            chevronLeftContainer = new BasicButton()
+            {
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreLeft,
+                Size = new Vector2(30, 20),
+                Position = new Vector2(15, 0),
+                Colour = Colour4.White,
+                Action = Previous,
+                Child = new FillFlowContainer()
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreLeft,
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Children = new Drawable[]
+                    {
+                        new SpriteIcon()
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Size = new Vector2(20),
+                            Icon = FontAwesome.Solid.ChevronLeft,
+                            Colour = Color4Extensions.FromHex("D2C9D8"),
+                        },
+                        new SpriteIcon()
+                        {
+                            Anchor = Anchor.CentreLeft,
+                            Origin = Anchor.CentreLeft,
+                            Size = new Vector2(20),
+                            Icon = FontAwesome.Solid.ChevronLeft,
+                            Colour = Color4Extensions.FromHex("D2C9D8"),
+                        }
+                    }
+                }
+            },
+            chevronRightContainer = new BasicButton()
+            {
+                Anchor = Anchor.CentreRight,
+                Origin = Anchor.CentreRight,
+                Size = new Vector2(30, 20),
+                Position = new Vector2(-15, 0),
+                Colour = Colour4.White,
+                Action = Next,
+                Child = new FillFlowContainer()
+                {
+                    Anchor = Anchor.CentreRight,
+                    Origin = Anchor.CentreRight,
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Children = new Drawable[]
+                    {
+                        new SpriteIcon()
+                        {
+                            Anchor = Anchor.CentreRight,
+                            Origin = Anchor.CentreRight,
+                            Size = new Vector2(20),
+                            Icon = FontAwesome.Solid.ChevronRight,
+                            Colour = Color4Extensions.FromHex("D2C9D8"),
+                        },
+                        new SpriteIcon()
+                        {
+                            Anchor = Anchor.CentreRight,
+                            Origin = Anchor.CentreRight,
+                            Size = new Vector2(20),
+                            Icon = FontAwesome.Solid.ChevronRight,
+                            Colour = Color4Extensions.FromHex("D2C9D8"),
+                        }
+                    }
+                }
+            },
+            new FillFlowContainer()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal,
+                Spacing = new Vector2(50, 0),
+                Children = new Drawable[]
+                {
+                    leftContainer2 = new TextureSwiperContainer(),
+                    leftContainer1 = new TextureSwiperContainer(),
+                    centerContainer = new TextureSwiperContainer()
+                    {
+                        Rotation = 10,
+                        Size = new Vector2(220, 220)
+                    },
+                    rightContainer1 = new TextureSwiperContainer(),
+                    rightContainer2 = new TextureSwiperContainer()
+                }
+            }
+        };
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Move to the next item.
+    /// </summary>
+    public void Next()
+    {
+        if (currentIndex.Value + 1 < Items.Count)
+        {
+            currentIndex.Value++;
+        }
+        else
+        {
+            currentIndex.Value = 0;
+        }
+
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Move to the previous item.
+    /// </summary>
+    public void Previous()
+    {
+        if (currentIndex.Value - 1 >= 0)
+        {
+            currentIndex.Value--;
+        }
+        else
+        {
+            currentIndex.Value = Items.Count - 1;
+        }
+
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Swipe to the first item.
+    /// </summary>
+    public void First()
+    {
+        currentIndex.Value = 0;
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Add <see cref="ISwiperItem{T}"/> to the swiper.
+    /// </summary>
+    /// <param name="item">The item that will be added.</param>
+    public void AddItem(TextureSwiperItem<T> item)
+    {
+        Items.Add(item);
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Remove <see cref="ISwiperItem{T}"/> from the swiper.
+    /// </summary>
+    /// <param name="item">The item that will be removed.</param>
+    public void RemoveItem(TextureSwiperItem<T> item)
+    {
+        Items.Remove(item);
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Set the index of the swiper item.
+    /// </summary>
+    /// <param name="index">The index that will be set.</param>
+    public void SetIndex(int index)
+    {
+        currentIndex.Value = index;
+        UpdateContainerItem();
+    }
+
+    /// <summary>
+    /// Get the current index of the swiper item.
+    /// </summary>
+    public int CurrentIndex => currentIndex.Value;
+
+    /// <summary>
+    /// Update the container item in the swiper.
+    /// </summary>
+    public void UpdateContainerItem()
+    {
+        // Update the left container 2.
+        // If current index - 2 is less than 0, set as blank container
+        if (currentIndex.Value - 2 < 0)
+        {
+            leftContainer2.ClearTexture();
+        }
+        else
+        {
+            leftContainer2.SetTexture(Items[currentIndex.Value - 2].Texture);
+        }
+
+        // Update the left container 1.
+        // If current index - 1 is less than 0, set as blank container
+        if (currentIndex.Value - 1 < 0)
+        {
+            leftContainer1.ClearTexture();
+        }
+        else
+        {
+            leftContainer1.SetTexture(Items[currentIndex.Value - 1].Texture);
+        }
+
+        // Update the center container.
+        centerContainer.SetTexture(Items[currentIndex.Value].Texture);
+
+        // Update the right container 1.
+        // If current index + 1 is more than the items count, set as blank container
+        if (currentIndex.Value + 1 >= Items.Count)
+        {
+            rightContainer1.ClearTexture();
+        }
+        else
+        {
+            rightContainer1.SetTexture(Items[currentIndex.Value + 1].Texture);
+        }
+
+        // Update the right container 2.
+        // If current index + 2 is more than the items count, set as blank container
+        if (currentIndex.Value + 2 >= Items.Count)
+        {
+            rightContainer2.ClearTexture();
+        }
+        else
+        {
+            rightContainer2.SetTexture(Items[currentIndex.Value + 2].Texture);
+        }
+
+        CurrentItem.Value = Items[currentIndex.Value].Item;
+    }
+
+    /// <summary>
+    /// Add <see cref="Action"/> that will be invoked when the index is changed.
+    /// </summary>
+    /// <param name="action"></param>
+    public void BindIndexChangeAction(Action<ValueChangedEvent<int>> action)
+    {
+        currentIndex.BindValueChanged(action);
+    }
+}
+
+/// <summary>
+/// The item that will be used in <see cref="HorizontalTextureSwiper{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of the item.</typeparam>
+public class TextureSwiperItem<T>
+{
+    public T Item { get; set; }
+
+    public Texture Texture { get; set; }
+}
+
+/// <summary>
+/// The container that will be use in <see cref="HorizontalTextureSwiper{T}"/>.
+/// </summary>
+public partial class TextureSwiperContainer : Container
+{
+    private readonly Sprite textureBox;
+
+    public TextureSwiperContainer()
+    {
+        Anchor = Anchor.Centre;
+        Origin = Anchor.Centre;
+        Size = new Vector2(70, 70);
+        Masking = true;
+        CornerRadius = 10;
+        FillMode = FillMode.Fill;
+        Child = textureBox = new Sprite()
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            RelativeSizeAxes = Axes.Both
+        };
+    }
+
+    /// <summary>
+    /// Set the texture of the container.
+    /// </summary>
+    /// <param name="texture">The texture that will be set.</param>
+    public void SetTexture(Texture texture)
+    {
+        textureBox.Texture = texture;
+    }
+
+    /// <summary>
+    /// Clear the texture of the container.
+    /// </summary>
+    public void ClearTexture()
+    {
+        textureBox.Texture = null;
+    }
+}
+
