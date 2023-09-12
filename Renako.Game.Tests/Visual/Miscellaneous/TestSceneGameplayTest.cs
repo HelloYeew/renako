765775f031dee758eabe74c51ef39960a553eafb
Phawit Pornwattanakul <me@helloyeew.dev>
2023-09-13 02:09:24 +0700

    Add note spawner and timer

--- Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
+++ Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio.Track;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Input.Events;
+using osu.Framework.Logging;
+using osu.Framework.Timing;
 using osu.Framework.Utils;
 using osuTK;
 using osuTK.Input;
@@ -30,6 +35,11 @@ public partial class TestSceneGameplayTest : RenakoTestScene
     private SpriteText characterPositionText;
     private SpriteText fruitPositionText;
     private SpriteText canGetScoreText;
+    private SpriteText clockText;
+
+    private StopwatchClock clock;
+
+    private Track track;
 
     private List<Key> controlKey = new List<Key>()
     {
@@ -43,8 +53,68 @@ public partial class TestSceneGameplayTest : RenakoTestScene
         Key.Up
     };
 
+    private List<double> fruitNoteTime = new List<double>()
+    {
+        2233.37,
+        3608.80,
+        4896.85,
+        6245.69,
+        7511.45,
+        8785.51,
+        10086.30,
+        11386.62,
+        23230.23,
+        25850.09,
+        28818.81,
+        29466.33,
+        31084.44,
+        32240.49,
+        33736.96,
+        36257.68,
+        39289.20,
+        41551.06,
+        44185.23,
+        46162.40,
+        46585.87,
+        48809.80,
+        50028.89,
+        51066.81,
+        51780.56,
+        53327.07,
+        55059.79,
+        56513.02,
+        57752.61,
+        59016.78,
+        60251.11,
+        61615.46,
+        62813.71,
+        64145.21,
+        65469.90,
+        66938.52,
+        68354.84,
+        69558.45,
+        70912.52,
+        72124.32,
+        3641.72,
+        74983.59,
+        76013.18,
+        77643.21,
+        79049.58,
+        0820.57,
+        82459.36,
+        84037.55,
+        85610.56
+    };
+
     private readonly int distancePerKey = 20;
     private readonly int hitWindow = 50;
+
+    [BackgroundDependencyLoader]
+    private void load(ITrackStore trackStore)
+    {
+        track = trackStore.Get("beatmaps/innocence-tv-size.mp3");
+        clock = new StopwatchClock();
+    }
 
     public TestSceneGameplayTest()
     {
@@ -128,21 +198,41 @@ public partial class TestSceneGameplayTest : RenakoTestScene
             Font = RenakoFont.GetFont(),
             Position = new Vector2(0, 60)
         });
+        Add(clockText = new SpriteText()
+        {
+            Anchor = Anchor.TopRight,
+            Origin = Anchor.TopRight,
+            Text = "Clock: 0",
+            Font = RenakoFont.GetFont(),
+            Position = new Vector2(0, 80)
+        });
 
         // randomly generate fruit in playfield in every 2 seconds in separate thread.
-        Scheduler.AddDelayed(() =>
+        // Scheduler.AddDelayed(() =>
+        // {
+        //     Add(fruit = new Circle()
+        //     {
+        //         Anchor = Anchor.Centre,
+        //         Origin = Anchor.Centre,
+        //         Size = new Vector2(20),
+        //         Position = new Vector2(RNG.NextSingle(-400, 400), RNG.NextSingle(-200, 200)),
+        //         Colour = Color4Extensions.FromHex("FF0000")
+        //     });
+        //     fruitPositionText.Text = $"Fruit Position: {fruit.Position.X}, {fruit.Position.Y}";
+        //     fruit.FadeTo(0, 5000).Expire();
+        // }, 5000, true);
+
+        AddStep("Toggle pause or play", () =>
         {
-            Add(fruit = new Circle()
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(20),
-                Position = new Vector2(RNG.NextSingle(-400, 400), RNG.NextSingle(-200, 200)),
-                Colour = Color4Extensions.FromHex("FF0000")
-            });
-            fruitPositionText.Text = $"Fruit Position: {fruit.Position.X}, {fruit.Position.Y}";
-            fruit.FadeTo(0, 5000).Expire();
-        }, 5000, true);
+            if (clock.IsRunning)
+                clock.Stop();
+            else
+                clock.Start();
+            if (track.IsRunning)
+                track.Stop();
+            else
+                track.Start();
+        });
 
         // TODO: We need more boolets
     }
@@ -216,6 +306,11 @@ public partial class TestSceneGameplayTest : RenakoTestScene
             }
         }
 
+        if (e.Key == Key.BackSpace)
+        {
+            Logger.Log(clock.CurrentTime.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
         return base.OnKeyDown(e);
     }
 
@@ -230,6 +325,29 @@ public partial class TestSceneGameplayTest : RenakoTestScene
             else
             {
                 canGetScoreText.Text = "Can get score: false";
+            }
+        }
+
+        clockText.Text = $"Clock: {clock.CurrentTime.ToString(CultureInfo.InvariantCulture)}";
+
+        // If clock is running, check if there is any note need to spawn (spawn note 3 second before it should be hit).
+        if (clock.IsRunning)
+        {
+            for (int i = 0; i < fruitNoteTime.Count; i++)
+            {
+                if (clock.CurrentTime >= fruitNoteTime[i] - 3000 && clock.CurrentTime <= fruitNoteTime[i] + 3000)
+                {
+                    Add(fruit = new Circle()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(20),
+                        Position = new Vector2(RNG.NextSingle(-400, 400), RNG.NextSingle(-200, 200)),
+                        Colour = Color4Extensions.FromHex("FF0000")
+                    });
+                    fruit.FadeTo(0, 3000).Expire();
+                    fruitNoteTime.RemoveAt(i);
+                }
             }
         }
 
