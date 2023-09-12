a5c1717b74acc8393974086a618732e5d8c35ad5
Phawit Pornwattanakul <me@helloyeew.dev>
2023-09-12 10:37:24 +0700

    Add mouse hover event

--- Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayNormalPhaseProofOfConcept.cs
+++ Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayNormalPhaseProofOfConcept.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
+using osu.Framework.Utils;
 using osuTK;
 using osuTK.Input;
 
@@ -14,6 +16,10 @@ namespace Renako.Game.Tests.Visual.Miscellaneous;
 public partial class TestSceneGameplayNormalPhaseProofOfConcept : RenakoTestScene
 {
     private Circle character;
+    private Triangle note;
+    private List<Triangle> notes = new List<Triangle>();
+
+    private bool followMouse = false;
 
     public TestSceneGameplayNormalPhaseProofOfConcept()
     {
@@ -69,15 +75,59 @@ public partial class TestSceneGameplayNormalPhaseProofOfConcept : RenakoTestScen
                 }
             }
         });
+
+        // Randomly drop note from top to bottom.
+        Scheduler.AddDelayed(() =>
+        {
+            Triangle note;
+            Add(note = new Triangle()
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Size = new Vector2(40),
+                Position = new Vector2(RNG.Next(-300, 300), -200),
+                Colour = Colour4.Red
+            });
+            note.MoveToY(2000, 5000).Expire();
+        }, 500, true);
+
+        AddToggleStep("Follow mouse", b =>
+        {
+            followMouse = b;
+        });
     }
 
     protected override bool OnKeyDown(KeyDownEvent e)
     {
         if (e.Key == Key.Left || e.Key == Key.Right)
         {
-            character.MoveToX(character.X + (e.Key == Key.Left ? -20 : 20), 100, Easing.OutQuint);
+            character.MoveToX(character.X + (e.Key == Key.Left ? -30 : 30), 50, Easing.OutQuint);
         }
 
         return base.OnKeyDown(e);
+    }
+
+    protected override void Update()
+    {
+        // Remove expired notes.
+        foreach (var note in notes)
+        {
+            if (note.IsAlive)
+                continue;
+
+            notes.Remove(note);
+        }
+
+        base.Update();
+    }
+
+    protected override bool OnMouseMove(MouseMoveEvent e)
+    {
+        if (followMouse)
+        {
+            character.MoveToX(e.MousePosition.X - 200, 50, Easing.OutQuint);
+        }
+
+        return base.OnMouseMove(e);
     }
 }
