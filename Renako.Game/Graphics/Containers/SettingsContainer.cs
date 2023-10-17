1e413765b617a63fde127098b836a5d674e68cc9
Phawit Pornwattanakul <me@helloyeew.dev>
2023-10-17 13:32:43 +0700

    Fix time format of current time

--- Renako.Game/Graphics/Containers/SettingsContainer.cs
+++ Renako.Game/Graphics/Containers/SettingsContainer.cs
@@ -376,7 +376,7 @@ public partial class SettingsContainer : FocusedOverlayContainer
     {
         base.Update();
 
-        currentTimeText.Text = DateTime.Now.ToString("HH:mm:ss tt").ToUpper();
+        currentTimeText.Text = DateTime.Now.ToString("hh:mm:ss tt").ToUpper();
         runningTimeText.Text = $"You are fighting the boss for {DateTime.Now - startGameTime:hh\\:mm\\:ss} !";
         updateBatteryStatus();
     }
