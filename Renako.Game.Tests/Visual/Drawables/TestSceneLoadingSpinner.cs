6556240e3459047d0da4a12fe055208693c8b26f
Phawit Pornwattanakul <me@helloyeew.dev>
2023-10-29 04:25:48 +0700

    Add spinner drawable

---
+++ Renako.Game.Tests/Visual/Drawables/TestSceneLoadingSpinner.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using osu.Framework.Graphics;
+using osuTK;
+using Renako.Game.Graphics.Drawables;
+
+namespace Renako.Game.Tests.Visual.Drawables;
+
+[TestFixture]
+public partial class TestSceneLoadingSpinner : GameDrawableTestScene
+{
+    public TestSceneLoadingSpinner()
+    {
+        Add(new LoadingSpinner()
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Size = new Vector2(100)
+        });
+    }
+}
---
+++ Renako.Game/Graphics/Drawables/LoadingSpinner.cs
@@ -0,0 +1,46 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+
+namespace Renako.Game.Graphics.Drawables;
+
+public partial class LoadingSpinner : CompositeDrawable
+{
+    private SpriteIcon icon;
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        InternalChild = new Container()
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            RelativeSizeAxes = Axes.Both,
+            Children = new Drawable[]
+            {
+                icon = new SpriteIcon()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Colour = Colour4.FromHex("6F5E67"),
+                    RelativeSizeAxes = Axes.Both,
+                    Icon = FontAwesome.Solid.CircleNotch
+                }
+            }
+        };
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+        animate();
+    }
+
+    private void animate()
+    {
+        icon.RotateTo(0).Then()
+            .RotateTo(360, 1000, Easing.InOutSine)
+            .Loop();
+    }
+}
