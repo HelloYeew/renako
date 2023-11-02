4b77c33f6478da35da9d15dee324d00f97bbf3b9
Phawit Pornwattanakul <me@helloyeew.dev>
2023-11-03 00:18:27 +0700

    Replace test scene with a new drawable

---
+++ Renako.Game.Tests/Visual/Drawables/TestSceneAudioVisualizer.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics;
+using Renako.Game.Audio;
+using Renako.Game.Graphics.Drawables;
+
+namespace Renako.Game.Tests.Visual.Drawables;
+
+[TestFixture]
+public partial class TestSceneAudioVisualizer : GameDrawableTestScene
+{
+    [Resolved]
+    private RenakoAudioManager audioManager { get; set; }
+
+    public TestSceneAudioVisualizer()
+    {
+        Add(new AudioVisualizer()
+        {
+            Anchor = Anchor.BottomLeft,
+            Origin = Anchor.BottomLeft,
+            RelativeSizeAxes = Axes.Both
+        });
+    }
+
+    [BackgroundDependencyLoader]
+    private void load(AudioManager audioManagerSource)
+    {
+        audioManager.Track = audioManagerSource.Tracks.Get("theme/main-theme.mp3");
+        audioManager.Track.Looping = true;
+        audioManager.Track.Start();
+    }
+}
