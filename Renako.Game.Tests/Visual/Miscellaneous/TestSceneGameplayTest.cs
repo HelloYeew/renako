5aef50403f603ab1d533873464c933e59021fe69
Phawit Pornwattanakul <me@helloyeew.dev>
2024-05-14 11:02:48 +0700

    Add basic gameplay element

--- Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
+++ Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
@@ -3,27 +3,98 @@ using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
-using Renako.Game.Graphics;
+using osu.Framework.Input.Events;
+using osuTK;
+using osuTK.Input;
 
 namespace Renako.Game.Tests.Visual.Miscellaneous;
 
 public partial class TestSceneGameplayTest : GameDrawableTestScene
 {
+    private Container playfield;
+    private Circle player;
+
     [BackgroundDependencyLoader]
     private void load(TextureStore textureStore)
     {
         BackgroundScreenStack.ChangeBackground(textureStore.Get("Screen/fallback-beatmap-background.jpg"));
         BackgroundScreenStack.AdjustMaskAlpha(0.5f);
 
-        Add(new FillFlowContainer()
+        Add(playfield = new Container()
         {
             Anchor = Anchor.Centre,
             Origin = Anchor.Centre,
-            RelativeSizeAxes = Axes.Both,
-            Direction = FillDirection.Horizontal,
-            Spacing = new osuTK.Vector2(125, 0),
+            Size = new Vector2(800, 500),
+            Children = new Drawable[]
+            {
+                new FillFlowContainer()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    RelativeSizeAxes = Axes.Both,
+                    Direction = FillDirection.Horizontal,
+                    Spacing = new Vector2(75, 0),
+                    Children = new Drawable[]
+                    {
+                        createLane(),
+                        createLane(),
+                        createLane(),
+                        createLane()
+                    }
+                },
+                new Box()
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Colour = Color4Extensions.FromHex("E8DEEE").Opacity(0.5f),
+                    Size = new Vector2(800, 20),
+                    Position = new Vector2(0, 200)
+                },
+                player = new Circle()
+                {
+                    Anchor = Anchor.BottomCentre,
+                    Origin = Anchor.BottomCentre,
+                    Size = new Vector2(20),
+                    Colour = Color4Extensions.FromHex("DAB4E7"),
+                    Position = new Vector2(125, 0)
+                }
+            }
+        });
+    }
+
+    protected override bool OnKeyDown(KeyDownEvent e)
+    {
+        switch (e.Key)
+        {
+            case Key.D:
+                player.MoveTo(new Vector2(-125, 0), 100, Easing.Out);
+                break;
+
+            case Key.F:
+                player.MoveTo(new Vector2(-40, 0), 100, Easing.Out);
+                break;
+
+            case Key.J:
+                player.MoveTo(new Vector2(40, 0), 100, Easing.Out);
+                break;
+
+            case Key.K:
+                player.MoveTo(new Vector2(125, 0), 100, Easing.Out);
+                break;
+        }
+
+        return base.OnKeyDown(e);
+    }
+
+    private Container createLane()
+    {
+        return new Container()
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Width = 10,
+            Height = 400,
             Children = new Drawable[]
             {
                 new Box()
@@ -31,38 +102,21 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
                     Width = 10,
-                    Height = 0.6f,
+                    Height = 1,
                     RelativeSizeAxes = Axes.Y,
                     Colour = Color4Extensions.FromHex("E8DEEE")
                 },
-                new Box()
+                new Circle()
                 {
                     Anchor = Anchor.Centre,
                     Origin = Anchor.Centre,
-                    Width = 10,
-                    Height = 0.6f,
-                    RelativeSizeAxes = Axes.Y,
-                    Colour = Color4Extensions.FromHex("E8DEEE")
-                },
-                new Box()
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Width = 10,
-                    Height = 0.6f,
-                    RelativeSizeAxes = Axes.Y,
-                    Colour = Color4Extensions.FromHex("E8DEEE")
-                },
-                new Box()
-                {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Width = 10,
-                    Height = 0.6f,
-                    RelativeSizeAxes = Axes.Y,
+                    Width = 20,
+                    Height = 20,
+                    Position = new Vector2(0, -0.5f),
+                    RelativePositionAxes = Axes.Y,
                     Colour = Color4Extensions.FromHex("E8DEEE")
                 }
             }
-        });
+        };
     }
 }
