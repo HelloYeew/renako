d89569cd39c6e14a1411ff2c9bbf6d7b67d89bb1
Phawit Pornwattanakul <me@helloyeew.dev>
2024-05-16 14:01:34 +0700

    Add basic note spawn and scoring algorithm

--- Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
+++ Renako.Game.Tests/Visual/Miscellaneous/TestSceneGameplayTest.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework.Internal;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
@@ -6,9 +10,12 @@ using osu.Framework.Graphics.Pooling;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Input.Events;
+using osu.Framework.Timing;
 using osu.Framework.Utils;
 using osuTK;
 using osuTK.Input;
+using Renako.Game.Graphics;
+using Logger = osu.Framework.Logging.Logger;
 
 namespace Renako.Game.Tests.Visual.Miscellaneous;
 
@@ -17,8 +24,23 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
     private Container playfield;
     private Container drawablePlayfield;
     private Circle player;
+    private statistics stats = new statistics();
+    private RenakoSpriteText scoreText;
+    private RenakoSpriteText criticalText;
+    private RenakoSpriteText breakText;
+    private RenakoSpriteText hitText;
+    private RenakoSpriteText missText;
+    private RenakoSpriteText clockText;
 
     private DrawablePool<note> notePool;
+
+    private const int fade_in_time = 500;
+    private const int move_time = 750;
+
+    private StopwatchClock stopwatchClock = new StopwatchClock();
+
+    private static BeatmapNote[] beatmap_notes;
+    private static PlayfieldNote[] playfield_notes;
 
     [BackgroundDependencyLoader]
     private void load(TextureStore textureStore)
@@ -77,16 +99,81 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
             Name = "drawablePlayfield"
         });
 
-        AddStep("create note on lane 1", () => addNote(Lane.Lane1));
-        AddStep("create note on lane 2", () => addNote(Lane.Lane2));
-        AddStep("create note on lane 3", () => addNote(Lane.Lane3));
-        AddStep("create note on lane 4", () => addNote(Lane.Lane4));
-        AddStep("create random note", () => addNote((Lane)RNG.Next(0, 4)));
+        Add(new FillFlowContainer()
+        {
+            Anchor = Anchor.TopRight,
+            Origin = Anchor.TopRight,
+            Size = new Vector2(100, 0),
+            AutoSizeAxes = Axes.Y,
+            Direction = FillDirection.Vertical,
+            Spacing = new Vector2(0, 10),
+            Margin = new MarginPadding(10),
+            Children = new Drawable[]
+            {
+                scoreText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "Score: 0"
+                },
+                criticalText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "Critical: 0"
+                },
+                breakText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "Break: 0"
+                },
+                hitText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "Hit: 0"
+                },
+                missText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "Miss: 0"
+                },
+                clockText = new RenakoSpriteText()
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "0"
+                }
+            }
+        });
+
         AddSliderStep("scale", 0, 1, 1f, scale =>
         {
             playfield.Scale = new Vector2(scale);
             drawablePlayfield.Scale = new Vector2(scale);
         });
+
+        List<BeatmapNote> beatmapNotesTemp = new List<BeatmapNote>();
+
+        for (int i = 0; i < 25; i++)
+        {
+            beatmapNotesTemp.Add(new BeatmapNote
+            {
+                Lane = (Lane)RNG.Next(0, 4),
+                Time = (i + 1) * 1000
+            });
+        }
+
+        beatmap_notes = beatmapNotesTemp.ToArray();
+        playfield_notes = beatmap_notes.Select(PlayfieldNote.FromBeatmapNote).ToArray();
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+        stopwatchClock.Start();
     }
 
     protected override bool OnKeyDown(KeyDownEvent e)
@@ -95,22 +182,54 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
         {
             case Key.D:
                 player.MoveTo(new Vector2(-125, 0), 100, Easing.Out);
+                processHit(Lane.Lane1);
                 break;
 
             case Key.F:
                 player.MoveTo(new Vector2(-40, 0), 100, Easing.Out);
+                processHit(Lane.Lane2);
                 break;
 
             case Key.J:
                 player.MoveTo(new Vector2(40, 0), 100, Easing.Out);
+                processHit(Lane.Lane3);
                 break;
 
             case Key.K:
                 player.MoveTo(new Vector2(125, 0), 100, Easing.Out);
+                processHit(Lane.Lane4);
                 break;
         }
 
         return base.OnKeyDown(e);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        foreach (var note in playfield_notes)
+        {
+            // Count missed note
+            if (!note.IsHit && stopwatchClock.CurrentTime - note.Time > 200)
+            {
+                note.IsHit = true;
+                stats.Miss++;
+                updateScoreText();
+            }
+
+            if (note.IsDrawn)
+                continue;
+
+            if (stopwatchClock.CurrentTime >= note.Time - move_time - fade_in_time)
+            {
+                Logger.Log($"Adding note at {note.Time} (current time: {stopwatchClock.CurrentTime})");
+                addNote(note);
+                note.IsDrawn = true;
+            }
+        }
+
+        clockText.Text = stopwatchClock.CurrentTime.ToString("0.00");
     }
 
     private Container createLane()
@@ -146,12 +265,12 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
         };
     }
 
-    private void addNote(Lane lane)
+    private void addNote(PlayfieldNote playfieldNote)
     {
         if (!notePool.IsLoaded)
             return;
 
-        float x = getLaneX(lane);
+        float x = getLaneX(playfieldNote.Lane);
         note n = notePool.Get(noteObject =>
         {
             noteObject.Position = new Vector2(x, -200);
@@ -160,10 +279,10 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
 
         drawablePlayfield.Add(n);
 
-        n.ScaleTo(new Vector2(1), 500, Easing.OutCubic)
+        n.ScaleTo(new Vector2(1), fade_in_time, Easing.OutCubic)
          .FadeIn(500)
          .Then()
-         .MoveTo(new Vector2(x, 200), 750)
+         .MoveTo(new Vector2(x, 200), move_time)
          .Then()
          .MoveTo(new Vector2(x, 400), 250)
          .FadeOut(250);
@@ -189,6 +308,70 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
         };
     }
 
+    private class BeatmapNote
+    {
+        public Lane Lane { get; set; }
+        public double Time { get; set; }
+    }
+
+    private class PlayfieldNote
+    {
+        public Lane Lane { get; set; }
+        public double Time { get; set; }
+        public bool IsDrawn { get; set; }
+
+        public bool IsHit { get; set; }
+
+        public static PlayfieldNote FromBeatmapNote(BeatmapNote beatmapNote) =>
+            new PlayfieldNote { Lane = beatmapNote.Lane, Time = beatmapNote.Time };
+    }
+
+    private void processHit(Lane lane)
+    {
+        PlayfieldNote[] notes = playfield_notes.Where(n => !n.IsHit && n.Lane == lane && Math.Abs(stopwatchClock.CurrentTime - n.Time) <= 200).ToArray();
+
+        if (notes.Length == 0)
+            return;
+
+        foreach (var note in notes)
+        {
+            note.IsHit = true;
+
+            double diff = Math.Abs(stopwatchClock.CurrentTime - note.Time);
+
+            if (diff < 50)
+                stats.Critical++;
+            else if (diff < 100)
+                stats.Break++;
+            else if (diff < 200)
+                stats.Hit++;
+            else
+                stats.Miss++;
+
+            stats.Score = stats.Score + 1000 - diff;
+            updateScoreText();
+        }
+    }
+
+    private void updateScoreText()
+    {
+        scoreText.Text = $"Score: {stats.Score}";
+        criticalText.Text = $"Critical: {stats.Critical}";
+        breakText.Text = $"Break: {stats.Break}";
+        hitText.Text = $"Hit: {stats.Hit}";
+        missText.Text = $"Miss: {stats.Miss}";
+    }
+
+    private class statistics
+    {
+        public double Score { get; set; }
+
+        public int Critical { get; set; } // Less than 50ms
+        public int Break { get; set; } // More than 50ms but less than 100ms
+        public int Hit { get; set; } // More than 100ms but less than 200ms
+        public int Miss { get; set; } // More than 200ms
+    }
+
     private partial class note : PoolableDrawable
     {
         public note()
@@ -208,5 +391,7 @@ public partial class TestSceneGameplayTest : GameDrawableTestScene
                 Colour = Color4Extensions.FromHex("D9D9D9")
             };
         }
+
+        public PlayfieldNote PlayfieldNote { get; set; }
     }
 }
