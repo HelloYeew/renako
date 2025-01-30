fd9e7f57417d7cc735ea53b1d57b8a4dfbf04d65
Phawit Pornwattanakul <me@helloyeew.dev>
2025-01-30 16:04:14 +0700

    Seperate development storage and production

--- Renako.Desktop/Program.cs
+++ Renako.Desktop/Program.cs
@@ -6,9 +6,15 @@ namespace Renako.Desktop
 {
     public static class Program
     {
+#if DEBUG
+        private const string base_game_name = "RenakoDevelopment";
+#else
+        private const string base_game_name = "Renako";
+#endif
+
         public static void Main()
         {
-            using (GameHost host = Host.GetSuitableDesktopHost(@"Renako"))
+            using (GameHost host = Host.GetSuitableDesktopHost(base_game_name))
             using (osu.Framework.Game game = new RenakoGameDesktop())
             {
                 VelopackApp.Build().Run();
